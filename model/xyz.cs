47033cbb497b92379c242f1b1fa3c0af1a142d4a
andy99ge <zjdecjtu@163.com>
2020-08-04 14:12:00 +0800

    modify bone data struct,3d

---
+++ model/xyz.cs
@@ -0,0 +1,10 @@
+namespace myapp.model
+{
+    public class xyz
+    {
+        public float x;     //float 3个小数点
+        public float y; //float 3个小数点
+        public float z;	//float 3个小数点
+
+    }
+}
